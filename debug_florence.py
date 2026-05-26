354f51fe9e1bb21043174227da3494a231d5160e
Nico <nicolo.iacobone@gmail.com>
2026-05-26 18:20:21 +0200

    fixed florence 2 integration on top of sam3

--- debug_florence.py
+++ debug_florence.py
@@ -1,5 +1,6 @@
 import os
 from unittest.mock import patch
+import requests
 from transformers import AutoProcessor, AutoModelForCausalLM
 from transformers.dynamic_module_utils import get_imports
 from PIL import Image
@@ -15,6 +16,29 @@ def fixed_get_imports(filename: str | os.PathLike) -> list[str]:
     imports.remove("flash_attn")
     return imports
 
+def run_example(task_prompt, text_input=None):
+    if text_input is None:
+        prompt = task_prompt
+    else:
+        prompt = task_prompt + text_input
+    inputs = processor(text=prompt, images=image, return_tensors="pt").to('cuda', torch.float16)
+    generated_ids = model.generate(
+      input_ids=inputs["input_ids"].cuda(),
+      pixel_values=inputs["pixel_values"].cuda(),
+      max_new_tokens=1024,
+      early_stopping=False,
+      do_sample=False,
+      num_beams=3,
+    )
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+    parsed_answer = processor.post_process_generation(
+        generated_text, 
+        task=task_prompt, 
+        image_size=(image.width, image.height)
+    )
+
+    return parsed_answer
+
 with patch("transformers.dynamic_module_utils.get_imports", fixed_get_imports):
     model = AutoModelForCausalLM.from_pretrained(
         "microsoft/Florence-2-large",
@@ -23,7 +47,21 @@ with patch("transformers.dynamic_module_utils.get_imports", fixed_get_imports):
         attn_implementation="sdpa"
     ).to(device)
 
+# model = AutoModelForCausalLM.from_pretrained(
+#         "microsoft/Florence-2-large",
+#         torch_dtype=torch_dtype,
+#         trust_remote_code=True,
+#         attn_implementation="sdpa"
+#     ).to(device)
+
 processor = AutoProcessor.from_pretrained(
     "microsoft/Florence-2-large",
     trust_remote_code=True
 )
+
+url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg?download=true"
+image = Image.open(requests.get(url, stream=True).raw)
+
+task_prompt = '<OD>'
+results = run_example(task_prompt)
+print(results)
